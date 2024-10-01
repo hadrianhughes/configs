994f8c23fda9196efea4361d22f4598d811b1139
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-10-01 08:51:12 +0100

    Changes to Neovim config

--- init.lua
+++ init.lua
@@ -1,38 +1,50 @@
 vim.opt.number = true
 
--- Ignore case when searching, unless search term includes an uppercase
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Don't highlight previous search results
-vim.opt.hlsearch = true
-
-vim.opt.wrap = true
-
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 
-vim.opt.guicursor = "n-v-c-i:block"
+vim.opt.guicursor = 'n-v-c-i:block'
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+vim.opt.termguicolors = true
+
+-- Set up lazy
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, lazy = true, opts = ...},
-	{ "neovim/nvim-lspconfig", lazy = true }
+-- Set up dependencies
+require('lazy').setup({
+  { 'ellisonleao/gruvbox.nvim', priority = 1000 , config = true, lazy = false, opts = ...},
+	{ 'neovim/nvim-lspconfig', lazy = true },
+	{ 'kylechui/nvim-surround', lazy = true },
+	{ 'dhruvasagar/vim-table-mode', lazy = true },
+	{ 'f-person/git-blame.nvim', lazy = false },
+	{
+		'Julian/lean.nvim',
+		event = { 'BufReadPre *.lean', 'BufNewFile *.lean' },
+
+		dependencies = {
+			'neovim/nvim-lspconfig',
+			'nvim-lua/plenary.nvim',
+			-- you also will likely want nvim-cmp or some completion engine
+		},
+
+		-- see details below for full configuration options
+		opts = {
+			lsp = {},
+			mappings = true,
+		}
+	}
 })
 
-vim.opt.termguicolors = true
 vim.cmd.colorscheme('gruvbox')
-
-require("lspconfig").hls.setup({})
+require'nvim-surround'.setup{}
